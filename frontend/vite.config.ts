cff6ebf0a323724149f992da0b0673f31f7edf7d
SaimonNeupane <saimonbro00007@gmail.com>
2026-01-07 10:56:23 +0545

    feat:containerize client , server and add nginx reverse proxy and load balancer for backend server

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,14 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true, // You likely already have this or passed it via CLI
+    port: 5173,
+    // ADD THIS SECTION BELOW:
+    allowedHosts: [
+      "nonkeyword_frontend",
+      "localhost",
+      "aleph-frontend"
+    ]
+  }
 });
